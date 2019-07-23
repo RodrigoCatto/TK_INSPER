99019bbb618ef1ba96ef589a30d93ff99a70f1cb
rodrigo <rodrigo.catto7@gmail.com>
2019-07-23 10:31:21 -0300

    Adicionando denovo

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/rodrigo/catkin_ws/src
+# Build directory: /home/rodrigo/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("realsense-ros/realsense2_description")
+subdirs("tk_llc")
+subdirs("ddynamic_reconfigure")
+subdirs("razor_imu_9dof")
+subdirs("realsense-ros/realsense2_camera")
+subdirs("ydlidar")
